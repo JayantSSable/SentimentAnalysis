6c8900a9edab1177b0aa7bc4d07ba649ccb5f7a5
JayantSSable <jayantssable@gmail.com>
2013-10-15 19:51:53 +0530

    2nd commit

--- git.first/src/git/first/GitTest.java
+++ git.first/src/git/first/GitTest.java
@@ -3,6 +3,6 @@ package git.first;
 public class GitTest {
 	public static void main(String args[])
 	{
-		System.out.println("Running Git");
+		System.out.println("Running Git is cool thing");
 	}
 }
