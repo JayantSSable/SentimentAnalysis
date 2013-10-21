9e42d5662c6f0c7fa2e7d430147545faec7e945e
JayantSSable <jayantssable@gmail.com>
2013-10-21 21:48:50 +0530

    last time.

--- git.first/src/git/first/GitTest.java
+++ git.first/src/git/first/GitTest.java
@@ -3,6 +3,6 @@ package git.first;
 public class GitTest {
 	public static void main(String args[])
 	{
-		System.out.println("Running Git is cool thing but I'm sleeping now");
+		System.out.println("Running Git is cool thing but I'm sleeping now,and i ");
 	}
 }
